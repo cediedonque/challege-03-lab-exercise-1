07a87b22903eb3e608277b48c4d1dc328cafc273
Cedie Donque <ceeedang@gmail.com>
2018-08-07 17:34:24 +0800

    This is my challenge exercise

---
+++ app/src/main/java/com/example/pc002/a03_lab_exercise_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pc002.a03_lab_exercise_1;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.enter_msg);
+        editText.setText("");
+    }
+}
